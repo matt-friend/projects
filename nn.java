0c7bacc15503388a4be4e4a1a1972581ddc008d9
matt-friend <mattfriend00@gmail.com>
2019-02-24 19:47:26 +0000

    learning about ml

---
+++ nn.java
@@ -0,0 +1,27 @@
+public class Main{
+	public static void main(String[] args){
+	}
+}
+
+public class neural_network{
+
+	float[] input_layer;
+	float[] output_layer;
+	float[] hidden_layer;
+	float[][] weights_1;
+	float[][] weights_2;
+	float[] biases_1;
+	float[] biases_2;
+
+	neural_network(int i, int h, int o){
+		this.input_layer = new float[i];
+		this.output_layer = new float[o];
+		this.hidden_layer = new float[h];
+		this.weights_1 = new float[h][i];
+		this.weights_2 = new float[o][h];
+		this.biases_1 = new float[h]
+		this.biases_2 = new float[o]
+
+	
+
+
