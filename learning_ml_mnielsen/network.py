470d41f3ed17c148a1963899c394c71bfe005176
matt-friend <mattfriend00@gmail.com>
2019-02-26 23:46:52 +0000

    -learning ml

--- learning_ml_mnielsen/network.py
+++ learning_ml_mnielsen/network.py
@@ -20,18 +20,30 @@ class Network(object):
         return a
 
     def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
-        if test_data: n_test = len(test_data)
-        n = len(training_data)
-        for j in xrange(epochs):
-            random.shuffle(training_data)
-            mini_batches = [
-                    training_data[k:k+mini_batch_size]
-                    for k in xrange(0, n, mini_batch_size)]
+        if test_data: #optional, allows for evaluation of network training progress after each epoch
+            n_test = len(test_data)
+        n = len(training_data) #training data - tuples with input and corresponding desired outputs
+        for j in xrange(epochs): #epochs - number of epochs to train for
+            random.shuffle(training_data) #randomly shuffle the training data
+            mini_batches = [ #partition training data into mini batches
+                    training_data[k:k+mini_batch_size] #take mini-batch sized slice of training data
+                    for k in xrange(0, n, mini_batch_size)] #range of values incremented by mini-batch size up to training data size n
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
+                self.update_mini_batch(mini_batch, eta) #apply single step of gradient descent
             if test_data:
-                print "Epoch {0}: {1} / {2}".format(j, self.evaluate(test)data), n_test)
+                print "Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test)
             else:
                 print "Epoch {0} complete".format(j)
 
-
+    def update_mini_batch(self, mini_batch, eta):
+        #update weights and biases of a network by applying gradient descent using backpropagation to a single mini batch
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nable_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabbla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw 
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb 
+                       for b, nb in zip(self.biases, nabla_b)]
